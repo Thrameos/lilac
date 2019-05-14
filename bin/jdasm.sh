c02d0b8bdda57bcf73928d849486becc759314ac
Karl Nelson <nelson85@llnl.gov>
2019-05-14 14:29:37 -0700

    Getting closer.

--- bin/jdasm.sh
+++ bin/jdasm.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-if [ -z "$JASM_JAVA_HOME" ]; then
-	echo "Please set the variable JASM_JAVA_HOME to the java runtime's directory. You need at least JRE 1.8"
+unameOut="$(uname -s)"
+case "${unameOut}" in
+	CYGWIN*)    DIR=`cygpath -wa $(dirname "$0")`;;
+    *)        DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";;
+esac
+JASM_INSTALL_DIR="$DIR/../dist"
+
+if [ -z "$JAVA_HOME" ]; then
+	echo "Please set the variable JAVA_HOME to the java runtime's directory. You need at least JRE 1.8"
 else 
-	JASM_JAVA_CMD=$JASM_JAVA_HOME/bin/java
-	if [ -f $JASM_JAVA_CMD ]; then 
-		JASM_INSTALL_DIR=`dirname "$0"`
-		"$JASM_JAVA_CMD" -classpath "$JASM_INSTALL_DIR/jasm.jar" org.jasm.tools.Disassembler $*
+	JASM_JAVA_CMD=$JAVA_HOME/bin/java
+	if [ -f "$JASM_JAVA_CMD" ]; then 
+		"$JASM_JAVA_CMD" -classpath "$JASM_INSTALL_DIR/*" org.jasm.tools.Disassembler $*
 	else 
 		echo "Couldn't find $JASM_JAVA_CMD! Please adjust JASM_JAVA_HOME to the correct value."
 	fi
